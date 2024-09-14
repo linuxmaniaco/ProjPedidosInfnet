819ca4c1e1e8cc36336759001c4cea8f0264a29d
Eduardo <eduardo.silva@ipremios.com>
2024-09-14 00:05:15 -0300

    feat/ logback

--- src/main/java/com/example/pedidos/controller/pedidoController.java
+++ src/main/java/com/example/pedidos/controller/pedidoController.java
@@ -4,6 +4,7 @@ import com.example.pedidos.model.Pedido;
 import com.example.pedidos.service.ImpostoService;
 import com.example.pedidos.service.PedidoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +15,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Slf4j
 public class pedidoController {
     private final PedidoService pedidoService;
     private final ImpostoService impostoService;
     @PostMapping
     public ResponseEntity create(@RequestBody Pedido pedido){
+        log.info("Gerando pedido: {}", pedido);
         BigDecimal totalImposto = impostoService.getTotalImposto(pedido).totalImposto();
         BigDecimal valorSemImposto = pedidoService.calcularValorTotal(pedido);
         pedido.setTotalimposto(totalImposto);
